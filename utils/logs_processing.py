9785618fd36b06e51102b15531ba577ed3ee2574
Pranav Rao <pranavrao@Pranavs-MacBook-Pro.local>
2026-02-05 17:37:58 +0530

    moved logs processing functions to a new file

---
+++ utils/logs_processing.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import json
+
+
+class IdentityTypes:
+    PAN = "pan"
+    NAME = "fullName"
+    DL = "dlNumber"
+    RC = "vehicleRC"
+    PHONE = "mobile"
+    EMAIL = "email"
+    VOTER = "voterId"
+    ADDRESS = "completeAddress"
+
+
+class EventTypes:
+    PAN_PROFILE = "pan-profile"
+    PAN_ADVANCED = "pan-advanced"
+    DL = "dummy_dl"
+    RC = "dummy_rc"
+    PHONE = "dummy_phone"
+    EMAIL = "dummy_email"
+    VOTER = "dummy_voter"
+    ADDRESS = "dummy_address"
+
+
+def pan_profile_processing(logs: pd.DataFrame) -> pd.DataFrame:
+    """
+    Process the PAN profile logs.
+
+    """
+    result = pd.DataFrame()
+    result[IdentityTypes.PAN] = logs.merchantrequestbody.apply(
+        lambda x: json.loads(x).get("pan")
+    )
+    result["requestTimestamp"] = logs.requesttimestamp
+    result.sort_values(by="requestTimestamp", inplace=True)
+    return result.drop_duplicates(subset=[IdentityTypes.PAN], keep="last")
+
+
+def dl_processing(logs: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
+def phone_processing(logs: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
+def email_processing(logs: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
+def rc_processing(logs: pd.DataFrame) -> pd.DataFrame:
+    pass
